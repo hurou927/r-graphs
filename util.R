52778a7b96d64accbcdc93754f4ed48e46124e55
hurou927 <god.be.with.ye.fs@gmail.com>
2019-12-21 18:46:05 +0900

    facet and lm

--- util.R
+++ util.R
@@ -12,3 +12,19 @@ predictvals <- function(model, xvar, yvar, xrange = NULL, samples = 100, ...) {
   newdata[[yvar]] <- predict(model, newdata = newdata, ...)
   newdata
 }
+
+
+
+lm_labels <- function(dat, x, y) {
+  mod <- lm(as.formula(sprintf("%s ~ %s",y, x)), data=dat)
+  formula <- if (is.na(coef(mod)[1]) || is.na(coef(mod)[2]) ) {
+    "NA"  
+  } else {
+    sprintf("italic(y) == %.2f %+.2f * italic(x)", 
+            round(coef(mod)[1],2), round(coef(mod)[2], 2))
+  }
+  r <- cor(dat[[y]], dat[[x]])
+  
+  r2 <- if(is.na(r)) "NA" else sprintf("italic(R^2) == %.2f", r^2)
+  data.frame(formula = formula, r2 = r2, stringsAsFactors = FALSE)
+}
